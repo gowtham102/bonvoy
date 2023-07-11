f24b334d66651266f0a5d8c09741229ae360380f
gowtham102 <gowtham@imcrinox.com>
2023-07-11 15:15:50 +0530

    created new pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,9 @@ import { LoaderModule } from './SharedResources/Components/Loader/loader.module'
 import { AboutUsModule } from './about-us/about-us.module';
 import { Header2Component } from './header2/header2.component';
 import { WarrantyComponent } from './warranty/warranty.component';
+import { WarrantypolicyComponent } from './warrantypolicy/warrantypolicy.component';
+import { ShippingpolicyComponent } from './shippingpolicy/shippingpolicy.component';
+import { ReplacementpolicyComponent } from './replacementpolicy/replacementpolicy.component';
 // import { FaqComponent } from './faq/faq.component';
 // import { WarrantyModule } from './warranty/warranty.module';
 // import { ThankyouComponent } from './thankyou/thankyou.component';
@@ -38,6 +41,9 @@ import { WarrantyComponent } from './warranty/warranty.component';
     FooterComponent,
     HomeComponent,
     Header2Component,
+    WarrantypolicyComponent,
+    ShippingpolicyComponent,
+    ReplacementpolicyComponent,
     // FaqComponent,
     // WarrantyComponent,
     // ThankyouComponent
---
+++ src/app/replacementpolicy/replacementpolicy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-replacementpolicy',
+  templateUrl: './replacementpolicy.component.html',
+  styleUrls: ['./replacementpolicy.component.css']
+})
+export class ReplacementpolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
